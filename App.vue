d0dec43e3825c70dda05c026a2e97d5ec6040e0a
Romanus <trueromanus@gmail.com>
2021-03-28 15:35:59 +0300

    Added scaffold for documentation website

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="app-container">
+        <app-menu>
+
+        </app-menu>
+        <div class="content-container">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default async function () {
+    require.resolveStyles(`https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css`);
+    require.resolveStyles(`https://use.fontawesome.com/releases/v5.0.13/css/all.css`);
+    await globalComponent(`https://p-rcollaboration.github.io/CoreComponents/src/states/FontIconState.vue`);
+    await globalComponent(`https://p-rcollaboration.github.io/CoreComponents/src/views/fontawesome/FontIconView.vue`);
+    const AppMenu = await remoteComponent(`AppMenu.vue`);
+
+    return {
+        name: `App`,
+        data() {
+            return {
+            }
+        },
+        methods: {
+        },
+        components: {
+            AppMenu
+        }
+    }
+}
+</script>
+
+<style>
+.app-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.content-container {
+    width: 800px;
+}
+</style>
---
+++ AppMenu.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="menu-container">
+        <div v-for="(group, index) in groups" :key="index">
+            <div
+                class="group-container"
+                @click="group.opened = !group.opened">
+                <span>{{ group.title }}</span>
+                <font-icon-state
+                    group="fas"
+                    :id="group.opened ? `fa-chevron-up` : `fa-chevron-down`"
+                    size="16px">
+                    <template #default="{ context }">
+                        <font-icon-view :context="context"></font-icon-view>
+                    </template>
+                </font-icon-state>
+            </div>
+            <div
+                class="option-container"
+                v-show="group.opened">
+                <div
+                    v-for="(option, optionIndex) in group.options"
+                    :key="optionIndex"
+                    @click="selectedItem = option">
+                    <span
+                        :class="{ 'selected-item': selectedItem === option }">
+                        {{ option.title }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default async function () {
+    return {
+        name: `AppMenu`,
+        data() {
+            return {
+                selectedItem: null,
+                groups: [
+                    {
+                        title: `Getting started`,
+                        opened: false,
+                        options: [
+                            {
+                                title: `Introduction`
+                            },
+                            {
+                                title: `Install`
+                            },
+                            {
+                                title: `Using CLI`
+                            },
+                        ]
+                    },
+                    {
+                        title: `Components`,
+                        opened: false,
+                        options: [
+                            {
+                                title: `Button`
+                            },
+                            {
+                                title: `RadioButton`
+                            },
+                            {
+                                title: `FontIcon`
+                            },
+                            {
+                                title: `DropDown`
+                            },
+                            {
+                                title: `CheckBox`
+                            },
+                            {
+                                title: `CheckBoxTriState`
+                            },
+                            {
+                                title: `TextBox`
+                            },
+                            {
+                                title: `NumberBox`
+                            },
+                            {
+                                title: `TextArea`
+                            },
+                            {
+                                title: `Slider`
+                            },
+                        ]
+                    },
+                    {
+                        title: `Layouts`,
+                        opened: false,
+                        options: [
+                            {
+                                title: `ListBox`
+                            }
+                        ]
+                    },
+                    {
+                        title: `Validation`,
+                        opened: false,
+                        options: [
+                            {
+                                title: `Introduction`
+                            },
+                            {
+                                title: `ValidateHost`
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.group-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    user-select: none;
+}
+.group-container > span {
+    margin-right: 8px;
+    font-weight: 600;
+    justify-self: stretch;
+    font-size: 1rem;
+    flex: 1;
+}
+.menu-container {
+    margin: 10px;
+    width: 140px;
+}
+.selected-item {
+    font-weight: bold;
+}
+.option-container {
+    padding-left: 4px;
+    font-size: 0.9rem;
+    user-select: none;
+}
+</style>
